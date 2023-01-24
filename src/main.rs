73b87af001f714e1c7fdf709604aa66b5d3f382e
kazuyukiy <tsuzuki.kazuyuki@gmail.com>
2023-01-24 21:23:37 +0900

    threadpool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod thread_pool;
+// mod wc_handler5;
+// mod swimming;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    //    wc_handler5::system_ini();
+    let pool = thread_pool::ThreadPool::new(4);
 }
