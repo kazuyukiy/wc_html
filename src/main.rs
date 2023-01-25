5f2c6fef6a95294074b4e5f657b4e9e25066745e
kazuyukiy <tsuzuki.kazuyuki@gmail.com>
2023-01-25 14:26:52 +0900

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 mod thread_pool;
 mod wc_handler;
 // mod swimming;
@@ -22,5 +26,32 @@ fn main() {
     // shop::shop();
 
     wc_handler::system_ini();
+
+    let listener = match TcpListener::bind("127.0.0.1:8080") {
+        Ok(v) => v,
+        Err(_) => return,
+    };
+
     let pool = thread_pool::ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        // let stream = stream.unwrap();
+        let stream = match stream {
+            Ok(v) => v,
+            Err(_) => continue,
+        };
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
 }
+
+fn handle_connection(mut stream: TcpStream) {
+    // println!("main.rs fn handle_connection");
+
+    // Consider to reject access from wher not local
+    let response = wc_handler::response(&mut stream);
+    stream.write(&response).unwrap();
+    stream.flush().unwrap();
+} // end of fn handle_connection
