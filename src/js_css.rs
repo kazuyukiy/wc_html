d1a524d739a2b5d23644b71df86714ebd5224238
kazuyukiy <tsuzuki.kazuyuki@gmail.com>
2023-03-01 21:04:45 +0900

    wc.js.022

--- src/js_css.rs
+++ src/js_css.rs
@@ -1,6 +1,3 @@
-use std::fs::File;
-use std::io::Write;
-
 mod css_source;
 mod js_source;
 
@@ -21,36 +18,5 @@ fn handle_dtype(dtype: &str) {
         _ => "",
     };
 
-    match std::fs::OpenOptions::new().write(true).open(&filename) {
-        Ok(mut file) => match write(&mut file, &source) {
-            Ok(_) => println!("wrote: {}", &filename),
-            Err(_) => println!("failed to write: {}", &filename),
-        },
-        // not exists
-        Err(_) => {
-            new_write(&filename, &source);
-        }
-    }
-}
-
-fn new_write(filename: &str, source: &str) {
-    match File::create(&filename) {
-        // new file
-        Ok(mut file) => match write(&mut file, source) {
-            Ok(_) => println!("write: {}", filename),
-            Err(_) => println!("failed to write : {}", filename),
-        },
-        Err(e) => {
-            println!("failed to create file: {}", filename);
-            println!("{}", e);
-            return;
-        }
-    };
-}
-
-fn write(file: &mut std::fs::File, source: &str) -> Result<(), ()> {
-    match file.write(&source.trim().as_bytes()) {
-        Ok(_) => Ok(()),
-        Err(_) => Err(()),
-    }
+    std::fs::write(&filename, &source);
 }
